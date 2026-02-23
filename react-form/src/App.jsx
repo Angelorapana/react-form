7ce826873ea0fa9fb57fbf5bbe0147d0520d8c17
angelorapana <angelorapana@icloud.com>
2026-02-23 11:29:06 +0100

    Reindering array in UI  con metodo map

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -3,14 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const list = [
   { id: 1, title: " Dazi, caos e ricorsi" },
-  { id: 2, title: " Milan, regalo all'inter" }
-  { id: 3, title: " Trump, l'incubo dell'attentato" }
-  { id: 4, title: " Arbitri altro FLOP!" }
+  { id: 2, title: " Milan, regalo all'inter" },
+  { id: 3, title: " Trump, l'incubo dell'attentato" },
+  { id: 4, title: " Arbitri altro FLOP!" },
 ];
 
 function App() {
   const [articles, setArticles] = useState(list)
   return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+
+          {list.map((element) => <div className="card card-body">
+            <ul>
+              <li key={element.id}> {element.title}</li>
+            </ul>
+          </div>)}
+
+        </div>
+      </div>
+    </div>
+
+
   )
 }
 
