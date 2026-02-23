b42d2b3af1adf625cd42c3a28b06917af2649c1d
angelorapana <angelorapana@icloud.com>
2026-02-23 11:23:30 +0100

    Creazione array e dichiarazione stato

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const list = [
-  { id: 1, title: " Dazi, caos e ricorsi" }
+  { id: 1, title: " Dazi, caos e ricorsi" },
   { id: 2, title: " Milan, regalo all'inter" }
   { id: 3, title: " Trump, l'incubo dell'attentato" }
   { id: 4, title: " Arbitri altro FLOP!" }
