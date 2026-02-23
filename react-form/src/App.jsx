b1508f2096ddd8ecdbfc00268bf5b45d8bc06a30
angelorapana <angelorapana@icloud.com>
2026-02-23 12:17:51 +0100

    Creazione form con aggiornamento di stato per il valore del form e blocco dell'evento di caricamento pagina al salvataggio

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -8,22 +8,38 @@ const list = [
   { id: 4, title: " Arbitri altro FLOP!" },
 ];
 
+
+function handleSubmit(event) {
+  event.preventDefault()
+}
+
 function App() {
   const [articles, setArticles] = useState(list)
+  const [text, setText] = useState("")    // utilizzo usestate per l'aggiornamento del testo dell'input del form 
+
   return (
     <div className="container">
       <div className="row">
-        <div className="col">
 
-          {list.map((element) => <div className="card card-body">
-            <ul>
-              <li key={element.id}> {element.title}</li>
-            </ul>
+
+        {articles.map((element) =>
+          <div key={element.id} className="col pt-3">
+            <div className="card card-body">
+              <ul className='list-unstyled'>
+                <li> {element.title}</li>
+              </ul>
+            </div>
           </div>)}
+        <form>
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onSubmit={handleSubmit} />
 
-        </div>
+        </form>
+
       </div>
-    </div>
+    </div >
 
 
   )
